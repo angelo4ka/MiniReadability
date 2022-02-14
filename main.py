ac6cbb553f032f7143099fabf8e7beaf789be8e5
angelo4ka <84062076+angelo4ka@users.noreply.github.com>
2022-02-15 00:00:05 +0300

    Изменена обработка ошибки ввода команды

--- main.py
+++ main.py
@@ -25,13 +25,13 @@ if __name__ == '__main__':
             #with open(f"html_articles\{name_file}.txt", "r", encoding = "utf-8") as f:
             #    while True:
             #        line = f.readline()
-                
+            #    
             #        if not line:
             #            break
                     
                     # Выводим строку (временная проверка)
-                    #line_tag_converter = TagConverter.strip_tags(line)
-                    #print(TagConverter.strip_tags(str(line)))
+            #        line_tag_converter = TagConverter.strip_tags(line)
+            #        print(TagConverter.strip_tags(str(line)))
         if len(command) == 1:
             print("Ошибка в синтаксисе команды.")
             print()
@@ -45,8 +45,13 @@ if __name__ == '__main__':
         print("Данная утилита формирует текстовый документ с полезной информацией из веб-страницы.")
         
         while not isExit:
-            try:
-                command = input("Введите команду: ")
+            number_of_spaces = 0
+            command = input("Введите команду: ")
+            for symbol in command:
+                if symbol == " ":
+                    number_of_spaces = number_of_spaces + 1
+
+            if command != "" and number_of_spaces != len(command):
                 dop_command = command
                 command = command.upper()
 
@@ -67,7 +72,7 @@ if __name__ == '__main__':
                 else:
                     commands_hdbk.CR_UnknownCommand(str(full_command))
                     print()
-            except:
+            else:
                 commands_hdbk.CR_CommandEntryError()
                 print()
 
