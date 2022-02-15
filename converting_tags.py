f1b02aaf3592b19ba0f8c8ad6befbf38a8b5ef26
angelo4ka <84062076+angelo4ka@users.noreply.github.com>
2022-02-16 01:22:49 +0300

    v0.3: Добавлена отчистка от пустого пространства
    Добавлена отчистка от лишних пробелов и переносов в тексте.
    
    Функции:
    - Программа производит "общение" с пользователем.
    - Программа удаляет html-теги текстового html-файла.
    - Программа удаляет лишние пробелы и переносы.
    
    Проблемы:
    - Файл не отчищается от тегов стиля и JavaScript.

--- converting_tags.py
+++ converting_tags.py
@@ -6,5 +6,36 @@ class TagConverter():
 
     def strip_tags(self, marker=""):
         re_tags = re.compile(r"<([^>]+)>", re.UNICODE)
+        text_re_tags = re_tags.sub(marker, self.text)
 
-        return re_tags.sub(marker, self.text)
+        with open(f"subtotal.txt", "w", encoding = "utf-8") as f_w:
+            f_w.write(text_re_tags)
+
+        with open(f"subtotal.txt", "r", encoding = "utf-8") as f_r:
+            my_list = f_r.readlines()
+            string = ""
+
+            for item in my_list:
+                spaces_score = 0
+                for i in item:
+                    if i == " ":
+                        spaces_score = spaces_score + 1
+                
+                if spaces_score != (len(item) - 1):
+                    words = item.split(' ')
+                    
+                    for word in words:
+                        if word != "":
+                            string = string + word
+                            if word != words[len(words)-1]:
+                                string = string + " "
+                
+            return string
+
+
+
+        #with open(f"html_articles\{name_file}.txt", "r", encoding = "utf-8") as f:
+        #        obj_tag_converter = TagConverter(f.read())
+        #print(' '.join([ t for t in  text_re_tags.split(' ') if t ]))
+        #return re.sub(r'\s+', ' ', text_re_tags)
+        #return re_tags.sub(marker, self.text)
